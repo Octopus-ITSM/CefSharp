e3e1d53179ad15ef85920e71de16103e3fe813b1
Sylvain Hamel <sylvainhamel0@gmail.com>
2014-10-28 11:25:53 -0400

    Fix: js bindings must be kept by browser, not by process

--- CefSharp.BrowserSubprocess.Core/CefAppUnmanagedWrapper.h
+++ CefSharp.BrowserSubprocess.Core/CefAppUnmanagedWrapper.h
@@ -21,9 +21,8 @@ namespace CefSharp
 	private:
 		gcroot<Action<CefBrowserWrapper^>^> _onBrowserCreated;
 		gcroot<Action<CefBrowserWrapper^>^> _onBrowserDestroyed;
-		gcroot<JavascriptRootObject^> _javascriptRootObject;
-		gcroot<Func<IBrowserProcess^>^> _createBrowserProxyDelegate;
 		gcroot<Dictionary<int, CefBrowserWrapper^>^> _browserWrappers;
+		CefBrowserWrapper^ FindBrowserWrapper(CefRefPtr<CefBrowser> browser, bool mustExist);
 	public:
 		
 		CefAppUnmanagedWrapper(Action<CefBrowserWrapper^>^ onBrowserCreated, Action<CefBrowserWrapper^>^ onBrowserDestoryed)
@@ -36,10 +35,8 @@ namespace CefSharp
 		~CefAppUnmanagedWrapper()
 		{
 			delete _browserWrappers;
-			delete _javascriptRootObject;
 			delete _onBrowserCreated;
 			delete _onBrowserDestroyed;
-			delete _createBrowserProxyDelegate;
 		}
 
 		virtual DECL CefRefPtr<CefRenderProcessHandler> GetRenderProcessHandler() OVERRIDE;
@@ -47,8 +44,6 @@ namespace CefSharp
 		virtual DECL void OnBrowserDestroyed(CefRefPtr<CefBrowser> browser) OVERRIDE;
 		virtual DECL void OnContextCreated(CefRefPtr<CefBrowser> browser, CefRefPtr<CefFrame> frame, CefRefPtr<CefV8Context> context) OVERRIDE;
 		virtual DECL void OnContextReleased(CefRefPtr<CefBrowser> browser, CefRefPtr<CefFrame> frame, CefRefPtr<CefV8Context> context) OVERRIDE;
-
-		void Bind(JavascriptRootObject^ rootObject, Func<IBrowserProcess^>^ createBrowserProxyDelegate);
 
 		IMPLEMENT_REFCOUNTING(CefAppUnmanagedWrapper);
 	};
